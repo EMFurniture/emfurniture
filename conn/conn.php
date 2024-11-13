29c73fb6e10ce0bbb282a97a07fa23602e1e7d8f
Andrew Tubalinal <john.tubalinal@gmail.com>
2024-11-13 12:27:13 +0800

    bugfix

--- conn/conn.php
+++ conn/conn.php
@@ -1,6 +1,6 @@
 <?php
 // Database credentials
-$servername = "your_database_host"; // Replace with your Hostinger hostname
+$servername = "emfurniture.shop"; // Replace with your Hostinger hostname
 $username = "u598048325_root";
 $password = "2yJ@kqOwC#";
 $dbname = "u598048325_em_furniture";
