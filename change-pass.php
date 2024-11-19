0906ae7e89de1fd059b0674684bf96c9a87a6b31
Andrew Tubalinal <john.tubalinal@gmail.com>
2024-11-19 19:41:21 +0800

    update

---
+++ change-pass.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+
+<head>
+    <title>EM Furniture</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="favicon" href="favicon.ico" type="x/favicon">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Julee|Koulen' rel='stylesheet'>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="sub-wrapper">
+            <div class="web-head">
+                <div class="logo">
+                    <img src="/assets/logo.svg" width="240px">
+                </div>
+                <!-- <div class="login" ng-controller="mainController">
+                    <div ng-show="!isLoggedIn" class="login-form">
+                        <form>
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="username" ng-model="formData.username"
+                                    placeholder="Username" required>
+                                <input type="password" class="form-control" id="password" ng-model="formData.password"
+                                    placeholder="Password" required>
+                                <div class="main-login-btn">
+                                    <button type="button" class="login-btn" ng-click="login()">Login</button>
+                                    <button type="button" class="signup-btn" ng-click="signup()">Sign Up</button>
+                                </div>
+                            </div>
+                        </form>
+
+                    </div>
+                </div> -->
+                <div class="loggedInNav">
+                    <ul>
+                        <li>
+                            <img src="/assets/shop.svg" alt="">
+                            <a href="shop.php">Shop</a>
+                        </li>
+                        <li>
+                            <img src="/assets/wishlist.svg" alt="">
+                            <a href="wishlist.php">Wishlist</a>
+                        </li>
+                        <li>
+                            <img src="/assets/cart.svg" alt="">
+                            <a href="cart.php">Cart</a>
+                        </li>
+                        <li>
+                            <img src="/assets/user.svg" alt="">
+                            <a href="account.php">Account</a>
+                        </li>
+                        <li>
+                            <img src="/assets/logout.svg" alt="">
+                            <a href="#">Logout</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="navigation" ng-controller="NavigationController">
+                <div class="nav-menu">
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="about.php">About Us</a></li>
+                        <li><a href="gallery.php">Gallery</a></li>
+                        <li href="contact"><a href="contact.php">Contact Us</a></li>
+                    </ul>
+                </div>
+                <hr>
+            </div>
+            
+            <div class="account-container">
+                <div class="ac-left-container">
+                    <img src="/assets/sample-boy.jpg" alt="">
+                </div>
+                <div class="ac-right-container">
+                    <h3>Change Password</h3>
+                    <div class="acrc-sub-container">
+                        <form action="">
+                            <div>
+                                <h5>Current Password:</h5>
+                                <input type="password">
+                            </div>
+                            <div>
+                                <h5>Enter New Password:</h5>
+                                <input type="password">
+                            </div>
+                            <div>
+                                <h5>Re-enter New Password:</h5>
+                                <input type="password">
+                            </div>
+                        </form>
+                        <div class="update-acc-btn"><a href="account.php">Change Password</a></div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="footer-container">
+            <div class="footer-wrapper">
+                <div class="footer-logo-img">
+                    <img src="/assets/logo.svg" width="240px">
+                </div>
+                <div class="em-contact-container">
+                    <div class="direct-contact">
+                        <div class="dc-container">
+                            <img src="/assets/footer/phone.svg" alt="">
+                            <p>(+63) 950-613-3018</p>
+                        </div>
+                        <div class="dc-container">
+                            <img src="/assets/footer/location.svg" alt="">
+                            <p>Pawili Bula, Camarines Sur</p>
+                        </div>
+                        <div class="dc-container">
+                            <img src="/assets/footer/email.svg" alt="">
+                            <p>emfurnitureph@gmail.com</p>
+                        </div>
+                    </div>
+
+                    <div class="direct-social">
+                        <div class="ds-container">
+                            <img src="/assets/footer/facebook.svg" alt="">
+                            <p>Facebook</p>
+                        </div>
+                        <div class="ds-container">
+                            <img src="/assets/footer/instagram.svg" alt="">
+                            <p>Instagram</p>
+                        </div>
+                        <div class="ds-container">
+                            <img src="/assets/footer/x.svg" alt="">
+                            <p>Twitter / X</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="copyright">
+                    <hr>
+                    <span>Copyright &#169; All rights reserved 2024</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer></footer>
+</body>
+
+</html>
---
+++ transaction-history.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+
+<head>
+    <title>EM Furniture</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="favicon" href="favicon.ico" type="x/favicon">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Julee|Koulen' rel='stylesheet'>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="sub-wrapper">
+            <div class="web-head">
+                <div class="logo">
+                    <img src="/assets/logo.svg" width="240px">
+                </div>
+                <!-- <div class="login" ng-controller="mainController">
+                    <div ng-show="!isLoggedIn" class="login-form">
+                        <form>
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="username" ng-model="formData.username"
+                                    placeholder="Username" required>
+                                <input type="password" class="form-control" id="password" ng-model="formData.password"
+                                    placeholder="Password" required>
+                                <div class="main-login-btn">
+                                    <button type="button" class="login-btn" ng-click="login()">Login</button>
+                                    <button type="button" class="signup-btn" ng-click="signup()">Sign Up</button>
+                                </div>
+                            </div>
+                        </form>
+
+                    </div>
+                </div> -->
+                <div class="loggedInNav">
+                    <ul>
+                        <li>
+                            <img src="/assets/shop.svg" alt="">
+                            <a href="shop.php">Shop</a>
+                        </li>
+                        <li>
+                            <img src="/assets/wishlist.svg" alt="">
+                            <a href="wishlist.php">Wishlist</a>
+                        </li>
+                        <li>
+                            <img src="/assets/cart.svg" alt="">
+                            <a href="cart.php">Cart</a>
+                        </li>
+                        <li>
+                            <img src="/assets/user.svg" alt="">
+                            <a href="account.php">Account</a>
+                        </li>
+                        <li>
+                            <img src="/assets/logout.svg" alt="">
+                            <a href="#">Logout</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="navigation" ng-controller="NavigationController">
+                <div class="nav-menu">
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="about.php">About Us</a></li>
+                        <li><a href="gallery.php">Gallery</a></li>
+                        <li href="contact"><a href="contact.php">Contact Us</a></li>
+                    </ul>
+                </div>
+                <hr>
+            </div>
+            
+            <div class="transaction-history-container">
+                <div class="thc-header">
+                    <div class="thc-product">
+                        <span>Product</span>
+                    </div>
+                    <div class="thc-price">
+                        <span>Price</span>
+                    </div>
+                    <div class="thc-date">
+                        <span>Date of Purchase</span>
+                    </div>
+                    <div class="thc-status">
+                        <span>Status</span>
+                    </div>
+                    <div class="thc-reference">
+                        <span>Reference Number</span>
+                    </div>
+                </div>
+                <div class="thc-list">
+                    <div class="thc-product">
+                        <span>Sala Set</span>
+                    </div>
+                    <div class="thc-price">
+                        <span>₱2,997</span>
+                    </div>
+                    <div class="thc-date">
+                        <span>November 19, 2024</span>
+                    </div>
+                    <div class="thc-status">
+                        <span>Pending</span>
+                    </div>
+                    <div class="thc-reference">
+                        <span>4562434652344</span>
+                    </div>
+                </div>
+                <div class="thc-list">
+                    <div class="thc-product">
+                        <span>Sala Chair</span>
+                    </div>
+                    <div class="thc-price">
+                        <span>₱1,499</span>
+                    </div>
+                    <div class="thc-date">
+                        <span>November 21, 2024</span>
+                    </div>
+                    <div class="thc-status">
+                        <span>Successful</span>
+                    </div>
+                    <div class="thc-reference">
+                        <span>4562434322344</span>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="footer-container">
+            <div class="footer-wrapper">
+                <div class="footer-logo-img">
+                    <img src="/assets/logo.svg" width="240px">
+                </div>
+                <div class="em-contact-container">
+                    <div class="direct-contact">
+                        <div class="dc-container">
+                            <img src="/assets/footer/phone.svg" alt="">
+                            <p>(+63) 950-613-3018</p>
+                        </div>
+                        <div class="dc-container">
+                            <img src="/assets/footer/location.svg" alt="">
+                            <p>Pawili Bula, Camarines Sur</p>
+                        </div>
+                        <div class="dc-container">
+                            <img src="/assets/footer/email.svg" alt="">
+                            <p>emfurnitureph@gmail.com</p>
+                        </div>
+                    </div>
+
+                    <div class="direct-social">
+                        <div class="ds-container">
+                            <img src="/assets/footer/facebook.svg" alt="">
+                            <p>Facebook</p>
+                        </div>
+                        <div class="ds-container">
+                            <img src="/assets/footer/instagram.svg" alt="">
+                            <p>Instagram</p>
+                        </div>
+                        <div class="ds-container">
+                            <img src="/assets/footer/x.svg" alt="">
+                            <p>Twitter / X</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="copyright">
+                    <hr>
+                    <span>Copyright &#169; All rights reserved 2024</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer></footer>
+</body>
+
+</html>
